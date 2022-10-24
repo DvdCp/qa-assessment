701af4eff68a550d80f39a26179dd260cf1c20ad
Nicolò Riboni <nicolo.riboni@scalapay.com>
2022-10-24 16:02:50 +0200

    update api test

--- src/test/api/createUser.test.js
+++ src/test/api/createUser.test.js
@@ -3,9 +3,10 @@ const { makeAxiosRequestWithExpectedStatus } = require('../../utilities/common')
 describe('GIVEN valid data for a user', () => {
   const name = 'Alex';
   const balance = 1000;
+  let userId;
 
   describe('WHEN I create a new user using POST', () => {
-    const expectedStatus = 200;
+    const expectedStatus = 201;
     const requestParams = {
       url: 'http://localhost:8080/api/users',
       method: 'POST',
@@ -22,6 +23,16 @@ describe('GIVEN valid data for a user', () => {
     it('THEN the user created is returned in the response', () => {
       expect(createUserApiResponse.data.name).toBe(name);
       expect(createUserApiResponse.data.balance).toBe(balance);
+      userId = createUserApiResponse.data.id;
     });
+  });
+
+  afterAll(async () => {
+    const expectedStatus = 200;
+    const requestParams = {
+      url: `http://localhost:8080/api/users/${userId}`,
+      method: 'DELETE',
+    };
+    await makeAxiosRequestWithExpectedStatus(requestParams, expectedStatus)
   });
 });
